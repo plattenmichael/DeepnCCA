40949cc3eac3a6fe0b910c8a056cc379b4c81e76
Michael Plattén <44608513+plattenmichael@users.noreply.github.com>
2020-10-20 15:23:23 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from load_model_and_predict import Predictor
 
 #Dataloader was also used in the training of the algorithm, and thus had several functions. For predicting with the model, the below pathways are not being called.
 
-data_path_b, data_path_cc, image_path = '/path_to_images_to_be_segmented/*.jpeg'
+data_path_b = data_path_cc = image_path = '/path_to_images_to_be_segmented/*.jpeg'
 model_corpus_callosum = '/path_to_the_saved_model_corpus_callosum/model/'
 model_brain = '/path_to_the_saved_model_brain/model/'
 
